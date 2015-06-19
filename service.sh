16f73dd9bdb8215414cc7d31bc57f3d896f1cb10
xiao bing <xiaobing@joyveb.com>
2015-06-19 15:30:24 +0800

    Update service.sh
    不能使用pid文件呀，换个方式实现status

--- service.sh
+++ service.sh
@@ -17,36 +17,50 @@ base_dir=/opt/jumpserver
 PROC_NAME="jumpsever"
 lockfile=/var/lock/subsys/${PROC_NAME}
 
-JUMP_PID=/var/run/subsys/${PROC_NAME}.pid
-LOG_PID=/var/run/subsys/${PROC_NAME}_log.pid
-NODE_PID=/var/run/subsys/${PROC_NAME}_node.pid
+
 
 start() {
 	jump_start=$"Starting ${PROC_NAME} service:"
 	
 	if [ -f $lockfile ];then	
-		 echo "jumpserver  is running..."
-		 success "$jump_start"
+	
+	         nums=0
+		 
+                 for i in manage.py log_handler.py index.js;do
+                   ps aux | grep "$i" | grep -v 'grep' &> /dev/null && let nums+=1 
+                 done
+
+                 if [ "x$nums" == "x3" ];then
+                    success "$jump_start"
+                    echo "jumpserver  is running..."
+                 else
+                    rm -f "$lockfile"
+                    failure "$jump_start"
+                    echo
+                 fi
+	
+
 	else
-		 daemon python $base_dir/manage.py runserver 0.0.0.0:80 -p $JUMP_PID & >> $base_dir/jumpserver.log 2>&1 &
-		 daemon python $base_dir/log_handler.py -p $LOG_PID &> /dev/null 2>&1 &
-                 cd $base_dir/websocket/;daemon node index.js -p $NODE_PID  &> /dev/null 2>&1 &
+		 daemon python $base_dir/manage.py runserver 0.0.0.0:80 & > $base_dir/jumpserver.log 2>&1 &
+		 daemon python $base_dir/log_handler.py  &> /dev/null 2>&1 &
+                 cd $base_dir/websocket/;daemon node index.js  &> /dev/null 2>&1 &
                  sleep 2
 
 		 echo -n "$jump_start"
 		 nums=0
-         for i in manage.py log_handler.py index.js;do
-             ps aux | grep "$i" | grep -v 'grep' &> /dev/null && let nums+=1 
-         done
+		 
+                 for i in manage.py log_handler.py index.js;do
+                   ps aux | grep "$i" | grep -v 'grep' &> /dev/null && let nums+=1 
+                 done
 
-         if [ "x$nums" == "x3" ];then
-            success "$jump_start"
-            touch "$lockfile"
-            echo
-         else
-            failure "$jump_start"
-            echo
-         fi
+                 if [ "x$nums" == "x3" ];then
+                    success "$jump_start"
+                    touch "$lockfile"
+                    echo
+                 else
+                    failure "$jump_start"
+                    echo
+                 fi
 		
 	 fi
 		
@@ -88,13 +102,21 @@ restart(){
 }
 
 status_fn() {
-  if [ -f $JUMP_PID ] && checkpid `cat $JUMP_PID` ; then
-    echo "jumpserver is running."
-    exit 0
-  else
-    echo "jumpserver is stopped."
-    exit 1
-  fi
+	
+	         nums=0
+		 
+                 for i in manage.py log_handler.py index.js;do
+                   ps aux | grep "$i" | grep -v 'grep' &> /dev/null && let nums+=1 
+                 done
+
+                 if [ "x$nums" == "x3" ];then
+                    echo "jumpserver  is running..."
+                    exit 0
+                 else
+                    rm -f "$lockfile"
+                    echo "jumpserver is stopped."
+                    exit 1
+                 fi
 }
 
 # See how we were called.
